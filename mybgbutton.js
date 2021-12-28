870a66c62c5d617fbd51e6fb69c5c1592c71f323
KanonKC <kanonkce@gmail.com>
2021-12-29 00:49:38 +0700

    Audio Update

--- mybgbutton.js
+++ mybgbutton.js
@@ -1,7 +1,8 @@
 $(document).ready(function(){
     $("#bg").click(function(){
+      // $("body").toggleClass("active-btn")
       if ($("#bg").prop('checked')) {
-          alert('Change background color to light blue!');
+          // alert('Change background color to light blue!');
          $("body").css({"background-color": "lightblue"});
       } else {
          $("body").css({"background-color": "white"});
--- myjs.js
+++ myjs.js
@@ -1,8 +1,12 @@
 var about_us = document.getElementById("about-us")
-var amoungUs = ["Among Us","sugomA","SUS"]
+var audio = document.getElementById("audio")
+var test = new Audio("https://cdn.discordapp.com/attachments/784488193852571668/925417644407017472/imposter_is_sus.mp3")
+
+var amoungUs = ["Among Us","sutuobA","Magic String"]
 // var audio = document.getElementById("audio")
 
 about_us.addEventListener('mouseover',(test)=>{
+    test.play()
     about_us.textContent = amoungUs[Math.floor((Math.random()*amoungUs.length))]
     // audio.play()
 })
@@ -10,3 +14,5 @@ about_us.addEventListener('mouseover',(test)=>{
 about_us.addEventListener('mouseleave',(test)=>{
     about_us.textContent = "About Us"
 })
+
+
